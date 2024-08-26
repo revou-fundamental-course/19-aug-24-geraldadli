00ef19612cc3952d1d520e86238103201ed1ea0e
geraldadli <geraldadli4@gmail.com>
2024-08-26 23:08:43 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-//Ini javascript
+function validateForm() {
+    const form = document.forms['message-us-form'];
+    const name = form['name'].value;
+    const email = form['email'].value;
+    const phone = form['phone_number'].value;
+
+    let isValid = true;
+
+    if (name == '') {
+        document.getElementById("error-form").innerHTML = "Name cannot be empty!";
+        isValid = false;
+    } else {
+        document.getElementById("error-form").innerHTML = "";
+    }
+
+    if (email == '') {
+        document.getElementById("error-form").innerHTML += "<br>Email cannot be empty!";
+        isValid = false;
+    }
+
+    if (phone == '') {
+        document.getElementById("error-form").innerHTML += "<br>Phone number cannot be empty!";
+        isValid = false;
+    }
+
+    if (isValid) {
+        document.getElementById("current-time").innerHTML = new Date().toLocaleTimeString();
+        document.getElementById("full-name").innerHTML = name;
+        return false;
+    }
+
+    return isValid; // Return true if valid, otherwise false
+}
+
+
+function setName(name){
+    document.getElementById("name").innerHTML = name;
+    document.getElementById('error-form').innerHTML = ""
+}
